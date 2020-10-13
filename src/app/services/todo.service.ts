d498737e2ac34879dd4aeacc7ccd77af21f6a5d0
safwanvk <safwanvalakundil@gmail.com>
2020-10-13 14:45:38 +0530

    :hammer: Modified

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Output } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs'
 
@@ -17,24 +17,29 @@ const httpOptions = {
 export class TodoService {
   todoUrl:string = 'https://jsonplaceholder.typicode.com/todos';
   todosLimit = '?_limit=5';
-  
+
 
   constructor(private http:HttpClient) { }
-  
-  // Get todos 
+
+  // Get todos
   getTodos():Observable<Todo[]>{
     return this.http.get<Todo[]>(`${this.todoUrl}${this.todosLimit}`);
   }
 
   // Toggle completed
-  toggleCompleted(todo: Todo):Observable<any> {
+  toggleCompleted(todo:Todo):Observable<any> {
     const url = `${this.todoUrl}/${todo.id}`;
     return this.http.put(url, todo, httpOptions);
   }
 
   // Delete Todo
-  deleteTodo(todo: Todo):Observable<Todo> {
+  deleteTodo(todo:Todo):Observable<Todo> {
     const url = `${this.todoUrl}/${todo.id}`;
     return this.http.delete<Todo>(url, httpOptions);
+  }
+
+  // Add Todo
+  addTodo(todo:Todo):Observable<Todo> {
+    return this.http.post<Todo>(this.todoUrl, todo, httpOptions);
   }
 }
